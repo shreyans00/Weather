c1118da67c2fa54e3921e23ae482f4d8131ee328
Shreyans <Shreyansraj3@gmail.com>
2022-12-09 23:01:17 +0530

    d

---
+++ build/precache-manifest.20c349e41e24b5a50aad1dffc09f15e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47a2414bea7cb1d1f42d",
+    "url": "/weather/static/css/main.2b4240d1.chunk.css"
+  },
+  {
+    "revision": "47a2414bea7cb1d1f42d",
+    "url": "/weather/static/js/main.47a2414b.chunk.js"
+  },
+  {
+    "revision": "308a304c7f4658dcccae",
+    "url": "/weather/static/js/1.308a304c.chunk.js"
+  },
+  {
+    "revision": "177e1b728fb0d4ffdfea",
+    "url": "/weather/static/js/runtime~main.177e1b72.js"
+  },
+  {
+    "revision": "0b15599b17e12dd8d6909717b22d2850",
+    "url": "/weather/index.html"
+  }
+];
